66b9adeed7fc26ef31988fbc51eb0135cd991b7a
Patrick Battisti <1729patrick@gmail.com>
2019-07-30 21:35:01 -0300

    Página SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
+import logo from '~/assets/logo.png';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
-// import { Container } from './styles';
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
-    <View>
-      <Input />
-      <Input
-        icon="call"
-        style={{ marginTop: 30 }}
-        placeholder="Digite seu numero"
-      />
-      <Button>Entrar</Button>
-    </View>
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboarType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => console.log('clicou')}>
+            Acessar
+          </SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => console.log('clicou')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
   );
 }
