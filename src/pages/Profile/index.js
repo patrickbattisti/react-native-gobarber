c9e62f5f60561c3815afc144362b8c0b87df9a89
Patrick Battisti <1729patrick@gmail.com>
2019-07-31 00:27:05 -0300

    Estilização das rotas

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -7,7 +7,7 @@ import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return (
     <Background>
       <View />
@@ -15,9 +15,9 @@ export default function Dashboard() {
   );
 }
 
-Dashboard.navigationOptions = {
-  tabBarLabel: 'Agendamento',
+Profile.navigationOptions = {
+  tabBarLabel: 'Meu Perfil',
   tabBarIcon: ({ tintColor }) => (
-    <Icon name="event" size={20} color={tintColor} />
+    <Icon name="person" size={20} color={tintColor} />
   ),
 };
