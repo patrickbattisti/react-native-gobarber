36086573420beaee2845c072232d5be4dd4eb0b6
Patrick Battisti <1729patrick@gmail.com>
2019-07-30 21:02:07 -0300

    Botão e Input

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <View />;
+  return (
+    <View>
+      <Input />
+      <Input
+        icon="call"
+        style={{ marginTop: 30 }}
+        placeholder="Digite seu numero"
+      />
+      <Button>Entrar</Button>
+    </View>
+  );
 }
